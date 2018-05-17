aac8c8e9450b1040e245c31b2b16a3ece55c4c5a
Wendy She <whshiwen@gmail.com>
2018-05-17 15:32:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist={EdSheeran:'Think out loud',PeterPeter:'Tergerverse',JustinBieber:'Sorry'}
 function updatePlaylist(playlist,artistName,songTitle){playlist[`${artistName}`]=`${songTitle}`;return playlist}
-var playlist={EdSheeran:'Think out loud',PeterPeter:'Tergerverse',JustinBieber:'Sorry'}
+function removeFromPlaylist(playlist,artistName){playlist.
